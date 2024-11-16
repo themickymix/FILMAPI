abf97c217f31c77d527942d98ac21a13acf64be9
themickymix <micz.yalon@gmail.com>
2024-11-16 20:11:34 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,29 +8,31 @@ import SearchResult from "./pages/SearchResult";
 import Footer from "./components/Footer";
 function App() {
   return (
-    <Router
-      future={{
-        v7_startTransition: true,
-        v7_relativeSplatPath: true,
-      }}>
-      <Navbar />
-      <div className="mt-5 mx-4 md:px-[15%] ">
-        <Routes>
-          {/* Default Route */}
-          <Route path="/" element={<Trending />} />
+    <div className="bg-[#1D232A]">
+      <Router
+        future={{
+          v7_startTransition: true,
+          v7_relativeSplatPath: true,
+        }}>
+        <Navbar />
+        <div className="mt-5 mx-4 md:px-[15%] ">
+          <Routes>
+            {/* Default Route */}
+            <Route path="/" element={<Trending />} />
 
-          {/* Trending Page Route */}
-          <Route path="/trending" element={<Trending />} />
+            {/* Trending Page Route */}
+            <Route path="/trending" element={<Trending />} />
 
-          {/* Movie Detail Page Route */}
-          <Route path="/movie/" element={<Movie />} />
-          <Route path="/tv/" element={<TV />} />
-          <Route path="/result/:type/:id" element={<ResultInfo />} />
-          <Route path="/results/" element={<SearchResult/>} />
-        </Routes>
-    <Footer/>
-      </div>
-    </Router>
+            {/* Movie Detail Page Route */}
+            <Route path="/movie/" element={<Movie />} />
+            <Route path="/tv/" element={<TV />} />
+            <Route path="/result/:type/:id" element={<ResultInfo />} />
+            <Route path="/results/" element={<SearchResult />} />
+          </Routes>
+          <Footer />
+        </div>
+      </Router>
+    </div>
   );
 }
 
