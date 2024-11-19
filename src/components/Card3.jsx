36c98006add215cf3cc9edc4d19a43189337cab7
themickymix <micz.yalon@gmail.com>
2024-11-19 08:55:10 +0800

    Update Card3.jsx

--- src/components/Card3.jsx
+++ src/components/Card3.jsx
@@ -3,7 +3,7 @@ function Card3({ img }) {
   return (
     <div className="w-16 ">
       <img
-        className="h-16 w-16 p-1 object-cover brightness-90"
+        className="h-16 w-16 p-1  object-cover brightness-90"
         src={img}
         alt={name}
       />
